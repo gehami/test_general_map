30dc855f312bda14cfee69e328fc7919b15a6f35
Albert Gehami <gehami@alumni.stanford.edu>
2020-05-14 13:01:56 -0700

    getting rgdal to work attempt 3

--- app.R
+++ app.R
@@ -40,7 +40,10 @@ library(sp)
 library(rgeos)
 library(shinyjs)
 library(leaflet.minicharts)
-library(rgdal)
+if(!require(rgdal)){
+  install.packages('rgdal')
+  library(rgdal)
+}
 
 # put a message in console or server log; note this happens only when the app is started!
 cat("uiStub application started...\n")
